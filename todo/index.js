be6b6a5ee0820df9fcae57f92a07efbf25c49c67
Gatman Serghei <33670865+Mostenn@users.noreply.github.com>
2018-10-02 21:56:55 +0300

    Update index.js

--- todo/index.js
+++ todo/index.js
@@ -18,7 +18,7 @@ document.getElementById('add').addEventListener('click', function () {
 
 document.getElementById('item').addEventListener('keydown', function (e){
     var value = this.value;
-    if((e.code === 'Enter' || e.code === '66') && value){
+    if((e.code === 'Enter' || e.code === '13') && value){
         addItem(value);
     }
 });
