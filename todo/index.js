843fa558c07866feb24189439e78bd12e37777f9
Gatman Serghei <kupnu4.hq@gmail.com>
2018-10-02 17:06:26 +0300

    go?

--- todo/index.js
+++ todo/index.js
@@ -18,7 +18,7 @@ document.getElementById('add').addEventListener('click', function () {
 
 document.getElementById('item').addEventListener('keydown', function (e){
     var value = this.value;
-    if(e.code === 'Enter' && value){
+    if((e.code === 'Enter' || e.code === 'Go') && value){
         addItem(value);
     }
 });
